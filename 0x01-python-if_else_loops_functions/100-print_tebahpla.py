dad5476ea51202de958ce812741a0648a3cf249c
Tamara <tlumumba@gmail.com>
2023-08-09 13:03:36 -0700

    File 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,9 @@
 
 for i in range(122, 96, -1):
     print("{:c}".format(i), end="")
-    i -= 32
+    if i % 2 == 1:
+        i -= 32
+    else:
+        i += 32
     print("{:c}".format(i), end="")
     print(" ")
