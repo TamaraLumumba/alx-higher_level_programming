4c5bc00d765c2316bbe298821d4e29b2a50b87bb
Tamara <tlumumba@gmail.com>
2023-08-09 12:24:47 -0700

    File 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@
 def uppercase(s):
     for c in s:
         if 'a' <= c <= 'z':
-            upper_ascii = ord(c) - 32  # Convert lowercase ASCII to uppercase ASCII
+            upper_ascii = ord(c) - 32
             print("{:c}".format(upper_ascii), end="")
         else:
             print(c, end="")
-    print()  # Print a new line at the end
+    print() 
