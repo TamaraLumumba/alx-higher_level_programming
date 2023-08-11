da1807c6858dea9f56ae0b144fe12c637e01374d
Tamara <tlumumba@gmail.com>
2023-08-11 05:57:16 -0700

    File 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from calculator_1 import add
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+from calculator_1 import sub
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+
+from calculator_1 import mul
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} * {} = {}".format(a, b, sub(a, b)))
+
+from calculator_1 import div
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{} / {} = {}".format(a, b, sub(a, b)))
