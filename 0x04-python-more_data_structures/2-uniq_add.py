74c2c5d1c02ec270e3a5b63545b76671e9ab53d3
Tamara <tlumumba@gmail.com>
2023-08-18 02:40:18 -0700

    File 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,9 @@
 
 def uniq_add(my_list=[]):
     unique_integers = set()
-    
+
     for num in my_list:
         if num not in unique_integers:
             unique_integers.add(num)
-    
+
     return sum(unique_integers)
