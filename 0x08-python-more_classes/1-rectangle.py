a8d0a35e88af386bc22302641a5501634819fce8
Tamara <tlumumba@gmail.com>
2023-09-08 00:41:01 -0700

    File 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,3 +5,32 @@
 class Rectangle:
     """Represents a rectangle"""
     def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Type and Value Validation"""
+        if not isinstance(self, int):
+            raise TypeError("width muist be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    height.setter
+    def height(self, value):
+        if not isinstance(self, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("Height must be > 0")
+        self.__height
