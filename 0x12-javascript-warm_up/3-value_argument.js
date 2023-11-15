0939f1f0178cd0be5a3bf6d5795fbbad29db53c7
Tamara Lumumba <tlumumba@gmail.com>
2023-11-15 13:41:49 -0800

    File 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]; }
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
