9e120863469295f945f4c45a2f3d42c967954976
Tamara Lumumba <tlumumba@gmail.com>
2023-11-15 14:27:21 -0800

    File 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,4 @@
 
 const { argv } = require('process');
 const converted = Number(argv[2]);
-if (isNaN(converted)) { conole.log('Not a number'); } else { console.log('My number: ${converted}'); }
+if (isNaN(converted)) { conole.log('Not a number'); } else { console.log(`My number: ${converted}`); }
